197e3d6d6ac0d10bdaf05177a0e7ceeb391106b8
PrazolMalla <Prajwalmalla16@gmail.com>
2024-05-28 17:21:00 +0545

    Initial Project Setup with vuex,toast,tailwind

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -2,8 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './style.css'
+import store from './store'
+import ToastPlugin from 'vue-toast-notification'
 const app = createApp(App)
 
 app.use(router)
-
+app.use(store)
+app.use(ToastPlugin)
 app.mount('#app')

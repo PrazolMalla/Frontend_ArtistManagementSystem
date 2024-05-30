252dc0643e1c1da4717717598ffb26ea13c92348
nirmalpandey24 <nirmalpandey055@gmail.comgir config --global user.name nirmalpandey24git config --global user.email nirmalpandey055@gmail.com>
2024-05-30 16:34:38 +0545

    Login Created

--- Frontend/src/router/index.js
+++ Frontend/src/router/index.js
@@ -1,24 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginPage from '../views/LoginPage.vue';
-import SlideBar from '@/components/SlideBar.vue';
-import AdminData from '@/components/AdminData.vue';
 
 const routes = [
   {
-    path: '/LoginPage',
+    path: '/login',
     name: 'LoginPage',
     component: LoginPage
   },
-  {
-    path:'/SlideBar',
-    name: 'SlideBar',
-    component:SlideBar
-  },
-  {
-    path:'/AdminData',
-    name:'AdminData',
-    component:AdminData
-  },
+
 ];
 
 const router = createRouter({

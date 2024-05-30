aaef13b8585d5b59ff7d6f89f6e04067ad7c8a6b
Anuska-Shahi <anuskashahi11@gmail.com>
2024-05-30 17:07:16 +0545

    remove sidebar

--- Frontend/src/components/icons/IconHamBurger.vue
+++
@@ -1,4 +0,0 @@
-<template>
-    <svg fill="#ffffff" width="16px" height="18px" viewBox="0 0 1920 1920" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M1920 1411.412v225.882H0v-225.882h1920Zm0-564.706v225.882H0V846.706h1920ZM1920 282v225.882H0V282h1920Z" fill-rule="evenodd"></path> </g></svg>
-
-</template>
--- Frontend/src/router/index.js
+++ Frontend/src/router/index.js
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import SignUp from '../vue/SignUp.vue'
-import Sidebar from '../components/Sidebar.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,11 +8,6 @@ const router = createRouter({
       path: '/signup',
       name: 'SignUp',
       component: SignUp
-    },
-    {
-      path: '/sidebar',
-      name: 'SideBar',
-      component: Sidebar,
     },
   ]
 })

3c6081b58372263b4310d2192deece13e128bd28
Anuska-Shahi <anuskashahi11@gmail.com>
2024-05-30 16:56:10 +0545

    Add Sidebar

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,11 +1,16 @@
 <template>
-  <Sidebar/>
+  <div class="flex flex-row">
+    <Sidebar/>
+    <!-- <Navbar/> -->
+  </div>
+  
   <RouterView />
 </template>
 
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import Sidebar from './components/Sidebar.vue';
+import Navbar from './components/Navbar.vue';
 </script>
 
 
--- Frontend/src/router/index.js
+++ Frontend/src/router/index.js
@@ -1,15 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import SignUp from '../vue/SignUp.vue'
 import Sidebar from '../components/Sidebar.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/signup',
-      name: 'SignUp',
-      component: SignUp
-    },
     {
       path: '/sidebar',
       name: 'SideBar',

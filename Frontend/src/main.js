b2740f41d5c1967422082c63b079d053af5c13ed
PrazolMalla <Prajwalmalla16@gmail.com>
2024-05-31 15:24:56 +0545

    oh my vue icon added

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -4,9 +4,16 @@ import router from './router'
 import './style.css'
 import store from './store/store.js'
 import ToastPlugin from 'vue-toast-notification'
+import * as MdIcons from 'oh-vue-icons/icons/md'
+import { OhVueIcon, addIcons } from 'oh-vue-icons'
+
+const Fa = Object.values({ ...MdIcons })
+addIcons(...Fa)
 const app = createApp(App)
 
+app.use()
 app.use(router)
 app.use(store)
 app.use(ToastPlugin)
+app.component('v-icon', OhVueIcon)
 app.mount('#app')
--- Frontend/src/router/index.js
+++ Frontend/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import LoginPage from '../views/LoginPage.vue';
+import LoginPage from '../views/LoginPage.vue'
+import NavBar from '../components/NavBar.vue'
 
 const routes = [
   {
@@ -7,12 +8,16 @@ const routes = [
     name: 'LoginPage',
     component: LoginPage
   },
-
-];
+  {
+    path: '/navbar',
+    name: 'navbar',
+    component: NavBar
+  }
+]
 
 const router = createRouter({
   history: createWebHistory(),
   routes
-});
+})
 
-export default router;
+export default router

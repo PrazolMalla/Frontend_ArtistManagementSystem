81bae3010282b75e0f1f5594eabeff283d400e0f
phuyalbish <phuyalbish@gmail.com>
2024-06-05 15:03:10 +0545

    Edit PopUp Done

--- src/App.vue
+++ src/App.vue
@@ -7,31 +7,28 @@
 </template>
 
 <script setup>
-    import axios from 'axios';
-    import { jwtDecode } from 'jwt-decode';
-    import { onMounted } from 'vue';
     import { useRouter } from 'vue-router';
     const router = useRouter();
-    onMounted(async () => {
-        console.log(localStorage.getItem("refresh_token"))
-        if(localStorage.getItem("refresh_token")){
-                    axios.post(`http://127.0.0.1:8000/api/token/refresh/`, {'refresh': localStorage.getItem("refresh_token")}, {
-                    headers: {
-                    "content-Type": "application/json",
-                    }}).then(response =>{
-                        if(response.status == 200){
-                            localStorage.setItem("access_token",response.data.access);
-                            let data = jwtDecode(response.data.access)
-                            localStorage.setItem("userId", data.user_id)
-                            router.push('/')
-                        }
-                        else{
-                            router.push('/login')
+    // onMounted(async () => {
+    //     console.log(localStorage.getItem("refresh_token"))
+    //     if(localStorage.getItem("refresh_token")){
+    //                 axios.post(`http://127.0.0.1:8000/api/token/refresh/`, {'refresh': localStorage.getItem("refresh_token")}, {
+    //                 headers: {
+    //                 "content-Type": "application/json",
+    //                 }}).then(response =>{
+    //                     if(response.status == 200){
+    //                         localStorage.setItem("access_token",response.data.access);
+    //                         let data = jwtDecode(response.data.access)
+    //                         localStorage.setItem("userId", data.user_id)
+    //                         router.push('/')
+    //                     }
+    //                     else{
+    //                         router.push('/login')
 
-                        }
-                    })
-                }
-    });
+    //                     }
+    //                 })
+    //             }
+    // });
 
 </script>
 

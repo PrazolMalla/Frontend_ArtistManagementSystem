26076424abf9539963c936a40a78bd729140f588
phuyalbish <phuyalbish@gmail.com>
2024-06-05 23:10:53 +0545

    Listing Page Done

---
+++ src/components/manage/album/AddAlbum.vue
@@ -0,0 +1,198 @@
+<template>
+
+  <fieldset class="border border-slate-700 rounded-md fixed sm:w-[60vw] ml-0 lg:ml-10 bg-dark-primary-color overflow-hidden z-40 m-auto">
+    <legend class="ml-10 ">Add Album</legend>
+    <v-icon
+        name="fa-times"
+        fill="#302f31"
+        scale="1"
+        @click="closeAdd"
+        class="absolute right-3 cursor-pointer "
+      />
+    <div
+      class="form-container w-full p-10 h-full  flex flex-wrap justify-center gap-5 align-middle"
+    >
+      <div
+        v-for="album in userInputField"
+        :key="album.id"
+        class="w-full sm:w-5/12 text-secondary-color"
+      >
+        <label :for="album.name" class="text-sm font-helvetica text-primary-text-color pl-3">
+          {{ album.label }}
+        </label>
+        <input
+          :type="album.type"
+          :name="album.name"
+          @blur="validateField(album.name)"
+          v-model="user[album.name]"
+          class="p-2 focus:outline-none w-full h-10 mb rounded-3xl border border-black focus:border-hover-yellow focus:ring focus:ring-btn-yellow focus:ring-opacity-50 text-black"
+        />
+        <span v-if="formErrors[album.name]" class="text-orange-300 pl-3 text-sm">{{
+          formErrors[album.name]
+        }}</span>
+      </div>
+      <div class="w-full sm:w-2/12 text-secondary-color flex flex-col mt-2">
+        <label
+          for="img"
+          class="cursor-pointer albums-center p-2 text-sm text-gray-900 bg-gray-50 rounded-full focus-within:outline-none focus-within:border-hover-yellow focus-within:ring focus-within:ring-btn-yellow focus-within:ring-opacity-50"
+          >Album Profile Pic</label
+        >
+        <input type="file" id="img" name="profile" @change="handleFileChange" class="hidden" />
+
+        <span v-if="formErrors.profile" class="text-orange-300 mt-1 pl-3 block text-sm">{{
+          formErrors.profile
+        }}</span>
+      </div>
+
+      <div class="w-full sm:w-2/12 text-secondary-color flex flex-col mt-2">
+        <label
+          for="img"
+          class="cursor-pointer albums-center p-2 text-sm text-gray-900 bg-gray-50 rounded-full focus-within:outline-none focus-within:border-hover-yellow focus-within:ring focus-within:ring-btn-yellow focus-within:ring-opacity-50"
+          >File(mp3/mp4)</label
+        >
+        <input type="file" id="img" name="profile" @change="handleFileChange" class="hidden" />
+
+        <span v-if="formErrors.file" class="text-orange-300 mt-1 pl-3 block text-sm">{{
+          formErrors.file
+        }}</span>
+      </div>
+
+      
+
+      <div class="flex flex-col">
+        <select
+          v-model="user.album"
+          id="gender"
+          name="album"
+          class="rounded-3xl px-3 py-2 mt-2 border border-black text-black focus:outline-none focus:border-hover-yellow focus:ring focus:ring-btn-yellow focus:ring-opacity-50"
+        >
+          <option value="" disabled>Album</option>
+          <option v-for="album in albumData">{{ album.name }}</option>
+        </select>
+        <span v-if="formErrors.album" class="text-orange-300">{{ formErrors.album }}</span>
+      </div>
+
+      <div class="flex flex-col">
+        <select
+          v-model="album.genre"
+          id="gender"
+          name="genre"
+          class="rounded-3xl px-3 py-2 mt-2 border border-black text-black focus:outline-none focus:border-hover-yellow focus:ring focus:ring-btn-yellow focus:ring-opacity-50"
+        >
+          <option value="" disabled>Genre</option>
+          <option v-for="album in genreData">{{ album.name }}</option>
+        </select>
+        <span v-if="formErrors.genre" class="text-orange-300">{{ formErrors.genre }}</span>
+      </div>
+
+      <div class="w-full text-center">
+        <input type="checkbox" class="w-5" v-model="isArtist" @change="updateIsBand" />
+        <label for="isArtist" class="p-2 text-primary-text-color hover:text-secondary-color"
+          >Are you adding this album as (XYZ) Band?</label
+        >
+      </div>
+      <div class="w-full flex justify-center gap-2 align-middle">
+        <button
+          class="bg-btn-yellow h-10 w-2/6 hover:text-secondary-color text-slate-200 text-md rounded-full hover:border hover:bg-transparent border-secondary-color bg-secondary-color"
+          type="submit"
+          @click="addAlbum"
+        >
+          Add Album
+        </button>
+      </div>
+    </div>
+  </fieldset>
+</template>
+
+<script setup>
+import { ref, computed, onMounted } from 'vue';
+import { useStore } from 'vuex';
+import axios from 'axios';
+
+const user = ref({
+  name: '',
+  description: '',
+  language: '',
+  release_at: '',
+  album: '',
+  artist: '',
+  band: '',
+  genre: '',
+  img_profile: null,
+  album_file: null,
+});
+
+const userInputField = ref([
+  { id: '1', name: 'name', type: 'text', label: 'Name' },
+  { id: '2', name: 'description', type: 'text', label: 'Description' },
+  { id: '3', name: 'language', type: 'text', label: 'Language' },
+  { id: '4', name: 'release_at', type: 'date', label: 'Released At' }
+]);
+
+const formErrors = ref('');
+
+const store = useStore();
+const albumData = computed(() => store.state.albumData);
+const genreData = computed(() => store.state.genreData);
+import { defineProps, defineEmits } from 'vue';
+
+
+const emit = defineEmits(['close']);
+
+function closeAdd() {
+  emit('close');
+}
+
+onMounted(() => {
+  store.dispatch('setAlbumData');
+  store.dispatch('setGenreData');
+});
+
+function validateField(fieldName) {
+  formErrors.value[fieldName] = '';
+}
+
+function addAlbum() {
+  formErrors.value = {};
+  if (user.value.name.length < 5) {
+    formErrors.value.name = 'Username should be at least 5 characters long.';
+  }
+  if (user.value.description.length < 5) {
+    formErrors.value.description = 'Description should be at least 5 characters long.';
+  }
+
+  if (Object.keys(formErrors.value).length === 0) {
+    axios({
+      method: 'post',
+      url: 'http://127.0.0.1:8000/api/user/post/',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      data: {
+        email: user.value.email,
+        username: user.value.username,
+        password: user.value.password,
+        firstname: user.value.firstname,
+        lastname: user.value.lastname,
+        dob: user.value.dob,
+        gender: user.value.gender,
+        country: user.value.country,
+        img_profile: user.value.img_profile,
+        is_artist: user.value.is_artist,
+      },
+    })
+      .then((response) => {
+        console.log(response);
+        if (response.status === 200) {
+          this.$router.push('/dashboard');
+        }
+      })
+      .catch((err) => {
+        console.log(err.response.data);
+      });
+  }
+}
+
+</script>
+
+<style scoped></style>
--- src/views/manage/AlbumManage.vue
+++ src/views/manage/AlbumManage.vue
@@ -1,6 +1,19 @@
+
+
 <template>
   <PageLayout>
     <template #content>
+      <div v-if="is_blur"
+          class="fixed top-16 bggradientpopup w-screen h-screen z-40 flex flex-col justify-center gap-10 items-center"
+        ></div>
+      <AddAlbum v-if="is_OpenAdd" @close="toggleCloseAdd" />
+      <ManageConfirmDialogue v-if="is_OpenDelete" actionQuestion="Do yo want to delete XYZ?" actionConfirm="Confirm Delete" @close="toggleCloseDelete" />
+      <EditAlbum  v-if="is_OpenEdit" @close="toggleCloseEdit" />
+      <ManageConfirmDialogue v-if="is_OpenRestore" actionQuestion="Do yo want to restore XYZ?" actionConfirm="Confirm Restore" @close="toggleCloseRestore" />
+       <!-- <ManageConfirmDialogue v-if="is_OpenHide" actionQuestion="Do yo want to Hide XYZ?" actionConfirm="Confirm Hide" @close="toggleCloseHide" />
+       <ManageConfirmDialogue v-if="is_OpenDisable" actionQuestion="Do yo want to Disable XYZ?" actionConfirm="Confirm Disable" @close="toggleCloseDisable" /> -->
+      
+
       <div class="text-primary-text-color flex flex-col gap-2 w-full">
         <div class="flex h-screen">
           <div class="flex-1 mt-5">
@@ -13,7 +26,7 @@
                   <input
                     type="text"
                     class="text-sm border-none w-full p-2 bg-transparent focus:outline-none text-xsm text-primary-text-color placeholder:text-primary-text-color hidden sm:flex"
-                    placeholder="Search Music..."
+                    placeholder="Search Album..."
                   />
                   <v-icon
                     name="md-search"
@@ -24,6 +37,7 @@
                 </div>
                 <button
                   class="px-4 py-2 bg-secondary-color text-dark-primary-color rounded-full border-2 hover:bg-transparent hover:border-secondary-color hover:text-secondary-color"
+                  @click="toggleOpenAdd"
                 >
                   Add Album
                 </button>
@@ -32,75 +46,98 @@
 
             <div class="flex flex-col justify-between">
               <div
-                class="hidden sm:flex flex-row bg-transparent border-b border-b-primary-text-color"
+                class="hidden sm:flex flex-row bg-transparent"
               >
-                <div class="p-4 w-full font-semibold">Name</div>
-                <div class="p-4 w-1/6 font-semibold">Hide</div>
-                <div class="p-4 w-1/6 font-semibold">Disable</div>
-                <div class="p-4 w-1/3 font-semibold">Actions</div>
-              </div>
+                <div class="w-3/6  font-semibold">Name</div>
+                <div class="flex  w-full justify-around items-center">
 
+
+                <div class="font-semibold">Hide</div>
+                <div class="font-semibold">Disable</div>
+                <div class="font-semibold">Restore</div>
+                <div class="font-semibold">Edit</div>
+                <div class="font-semibold">Delete</div>
+                </div>
+              </div>
               <div
                 v-for="album in albums"
                 :key="album.name"
                 class="flex sm:flex-row flex-col items-center border-b border-b-primary-text-color cursor-pointer hover:bg-light-primary-color"
               >
-                <router-link :to="`/album/${album.id}`" class="p-4 flex items-center w-full">
+                <router-link  :to="`/album/${album.id}`" class="flex items-center w-3/6  ">
                   <img
                     :src="`http://127.0.0.1:8000${album.img_profile}`"
                     alt="Album image"
                     class="w-12 h-12 md:w-16 md:h-16 rounded-lg mr-4"
                   />
 
-                  <div>
+                  <div class="w-1-6">
                     <div class="font-bold text-secondary-color text-sm sm:text-base md:text-md">
                       {{ album.name }}
                     </div>
+                    <div class="flex flex-col sm:flex-row sm:gap-2">
+                      <div class="text-sm sm:text-base">{{ album.artist }}</div>
 
-                    <div class="text-sm sm:text-base">{{ album.artist }}</div>
+                      <div class="text-sm sm:text-base">{{ album.album }}</div>
+                    </div>
                   </div>
                 </router-link>
-
                 <div
                   class="flex w-full justify-around flex-row bg-transparent sm:hidden border-b border-b-primary-text-color"
                 >
                   <p>Hide</p>
-                  <p>Enable</p>
-                  <p>Actions</p>
+                  <p>Disable</p>
+                  <p>Restore</p>
+                  <p>Edit</p>
+                  <p>Delete</p>
                 </div>
-                <div class="flex justify-around w-full">
-                  <div class="p-4 w-1/6">
-                    <label class="relative inline-flex cursor-pointer items-center">
-                      <input id="switch-2" type="checkbox" class="peer sr-only" />
-                      <label for="switch-2" class="hidden"></label>
-                      <div
-                        class="peer h-4 w-11 rounded-full border bg-primary-text-color after:absolute after:-top-1 after:left-0 after:h-6 after:w-6 after:rounded-full after:border after:border-primary-text-color after:bg-white after:transition-all after:content-[''] peer-checked:bg-secondary-color peer-checked:after:translate-x-full"
-                      ></div>
-                    </label>
-                  </div>
-                  <div class="p-4 w-1/6">
-                    <label class="relative inline-flex cursor-pointer items-center">
-                      <input id="switch-2" type="checkbox" class="peer sr-only" />
-                      <label for="switch-2" class="hidden"></label>
-                      <div
-                        class="peer h-4 w-11 rounded-full border bg-primary-text-color after:absolute after:-top-1 after:left-0 after:h-6 after:w-6 after:rounded-full after:border after:border-primary-text-color after:bg-white after:transition-all after:content-[''] peer-checked:bg-secondary-color peer-checked:after:translate-x-full"
-                      ></div>
-                    </label>
-                  </div>
-                  <div class="p-4 w-1/3 flex gap-6">
-                    <v-icon
+                <div class="flex w-full justify-around items-center">
+                
+                    
+                          <label class="relative inline-flex cursor-pointer items-center">
+                          <input id="switch-2" type="checkbox" class="peer sr-only" />
+                          <label for="switch-2" class="hidden"></label>
+                          <div
+                          class="peer h-4 w-11 rounded-full border bg-primary-text-color after:absolute after:-top-1 after:left-0 after:h-6 after:w-6 after:rounded-full after:border after:border-primary-text-color after:bg-white after:transition-all after:content-[''] peer-checked:bg-secondary-color peer-checked:after:translate-x-full"
+                          ></div>
+                          </label>
+                          <label class="relative inline-flex cursor-pointer items-center">
+                          <input id="switch-2" type="checkbox" class="peer sr-only" />
+                          <label for="switch-2" class="hidden"></label>
+                          <div
+                          class="peer h-4 w-11 rounded-full border bg-primary-text-color after:absolute after:-top-1 after:left-0 after:h-6 after:w-6 after:rounded-full after:border after:border-primary-text-color after:bg-white after:transition-all after:content-[''] peer-checked:bg-secondary-color peer-checked:after:translate-x-full"
+                          ></div>
+                          </label>
+
+                    <!-- <div v-if="true">
+                      <div @click="toggleOpenHide" v-if="true" class="border border-secondary-color  rounded bg-secondary-color hover:text-secondary-color hover:bg-transparent text-sm p-1 text-dark-primary-color">Hide</div>
+                      <div v-if="false" class="border  border-secondary-color  rounded bg-transparent text-sm p-1 text-secondary-colo">Show</div>
+                    </div>
+
+                    <div v-if="true">
+                      <div @click="toggleOpenDisable" v-if="true" class="border border-secondary-color  rounded bg-secondary-color hover:text-secondary-color hover:bg-transparent text-sm p-1 text-dark-primary-color">Disable</div>
+                      <div v-if="false" class="border  border-secondary-color  rounded bg-transparent text-sm p-1 text-secondary-colo">Enable</div>
+                    </div> -->
+
+
+                    <div v-if="true">
+                      <div @click="toggleOpenRestore" v-if="true" class="border border-secondary-color  rounded bg-secondary-color hover:text-secondary-color hover:bg-transparent text-sm p-1 text-dark-primary-color">Restore</div>
+                      <div v-if="false" class="border  border-secondary-color  rounded bg-transparent text-sm p-1 text-secondary-colo">Restored</div>
+                    </div>
+                    
+                    <v-icon class=" cursor-pointer"
+                      @click="toggleOpenEdit"
                       name="fa-regular-edit"
                       fill="#00b166"
                       scale="1.5"
-                      class="cursor-pointer"
                     ></v-icon>
-                    <v-icon
+                    <v-icon class=" cursor-pointer"
+                      @click="toggleOpenDelete"
                       name="fa-regular-trash-alt"
                       fill="#ff4000"
                       scale="1.5"
-                      class="cursor-pointer"
                     ></v-icon>
-                  </div>
+                  
                 </div>
               </div>
             </div>
@@ -112,9 +149,76 @@
 </template>
 
 <script setup>
+import AddAlbum from '@/components/manage/album/AddAlbum.vue'
+import EditAlbum from '@/components/manage/album/EditAlbum.vue'
+import ManageConfirmDialogue from '@/components/manage/ManageConfirmDialogue.vue'
+
 import { ref, onMounted } from 'vue'
 import axios from 'axios'
 const albums = ref([])
+const is_blur = ref(false)
+const is_OpenAdd = ref(false)
+const is_OpenEdit = ref(false)
+const is_OpenDelete = ref(false)
+const is_OpenRestore = ref(false)
+// const is_OpenHide = ref(false)
+// const is_OpenDisable = ref(false)
+
+function toggleOpenAdd() {
+  is_OpenAdd.value = true
+  is_blur.value = true
+}
+function toggleCloseAdd() {
+  is_OpenAdd.value = false
+  is_blur.value = false
+}
+function toggleOpenEdit() {
+  is_OpenEdit.value = true
+  is_blur.value = true
+}
+function toggleCloseEdit() {
+  is_OpenEdit.value = false
+  is_blur.value = false
+}
+function toggleOpenDelete() {
+  is_OpenDelete.value = true
+  is_blur.value = true
+}
+function toggleCloseDelete() {
+  is_OpenDelete.value = false
+  is_blur.value = false
+}
+
+
+function toggleOpenRestore() {
+  is_OpenRestore.value = true
+  is_blur.value = true
+}
+function toggleCloseRestore() {
+  is_OpenRestore.value = false
+  is_blur.value = false
+}
+
+
+// function toggleOpenHide() {
+//   is_OpenHide.value = true
+//   is_blur.value = true
+// }
+// function toggleCloseHide() {
+//   is_OpenHide.value = false
+//   is_blur.value = false
+// }
+
+
+// function toggleOpenDisable() {
+//   is_OpenDisable.value = true
+//   is_blur.value = true
+// }
+// function toggleCloseDisable() {
+//   is_OpenDisable.value = false
+//   is_blur.value = false
+// }
+
 
 const fetchAlbums = async () => {
   try {
@@ -127,4 +231,15 @@ const fetchAlbums = async () => {
 }
 
 onMounted(fetchAlbums)
+
 </script>
+
+<style scoped>
+.bggradientpopup {
+  background: #ffffff3f;
+  backdrop-filter: blur(3px);
+  -webkit-backdrop-filter: blur(3px);
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  border: 1px solid rgba(255, 255, 255, 0.2);
+}
+</style>

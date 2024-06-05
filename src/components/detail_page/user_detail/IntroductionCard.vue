51f4910f8bc54f9c408e16540c8c24ac075a7a90
nirmalpandey24 <nirmalpandey055@gmail.comgir config --global user.name nirmalpandey24git config --global user.email nirmalpandey055@gmail.com>
2024-06-05 16:15:29 +0545

    With Przwaaal

--- src/components/detail_page/user_detail/IntroductionCard.vue
+++ src/components/detail_page/user_detail/IntroductionCard.vue
@@ -1,21 +1,27 @@
-<template lang="">
+<template>
   <div
     class="mt-5 w-[80vw] h-40 bg-light-primary-color p-5 md:w-56 md:h-40 md:mt-20 lg:mt-24 lg:w-96 lg:h-52 lg:p-6 rounded-md sm:mt-10"
   >
     <h1 class="font-semibold text-primary-text-color lg:text-lg">INTRODUCTION</h1>
     <div class="mt-2">
-      <p class="text-sm text-primary-text-color lg:text-lg">Name: {{ user.stageName }}</p>
-      <p class="mt-1 text-sm text-primary-text-color lg:text-lg">Country: {{ user.country }}</p>
-      <p class="mt-1 text-sm text-primary-text-color lg:text-lg">Bio: {{ user.bio }}</p>
+      <p class="text-sm text-primary-text-color lg:text-lg">Name: {{ userData.username }}</p>
+      <p class="mt-1 text-sm text-primary-text-color lg:text-lg">Country: {{ userData.country }}</p>
+      <p class="mt-1 text-sm text-primary-text-color lg:text-lg">Bio: {{ userData.bio }}</p>
     </div>
   </div>
 </template>
+
 <script setup>
-import { ref } from 'vue'
-const user = ref({
-  stageName: 'Prazol',
-  country: 'Nepal',
-  bio: 'Hello My name is Prazol Malla i am CSIT'
-})
+import { ref, onMounted ,defineProps} from 'vue'
+import axios from 'axios'
+
+const props = defineProps({
+  userData: {
+    type:Object,
+    required:true
+  }})
 </script>
-<style lang=""></style>
+
+<style scoped>
+/* Add your custom styles here */
+</style>
--- src/components/detail_page/user_detail/ProfilePicComponent.vue
+++ src/components/detail_page/user_detail/ProfilePicComponent.vue
@@ -1,9 +1,17 @@
 <template lang="">
     <div class="absolute lg:top-64 z-10 sm:w-72 sm:h-72 top-36 md:top-32 ">
-        <img src="https://source.unsplash.com/800x800/?portrait" alt="" class=" lg:w-44 lg:h-44 sm:w-32 sm:h-32  h-20 w-20  border-4 rounded-full mt-4 hover:cursor-pointer border-secondary-color">
+        <img :src="`http://127.0.0.1:8000${userImg}`" alt="" class=" lg:w-44 lg:h-44 sm:w-32 sm:h-32  h-20 w-20  border-4 rounded-full mt-4 hover:cursor-pointer border-secondary-color">
     </div>
 </template>
 <script setup>
+import { ref, onMounted ,defineProps} from 'vue'
+import axios from 'axios'
+
+const props = defineProps({
+  userImg: {
+    type:String,
+    required:true
+  }})
 </script>
 <style lang="">
     

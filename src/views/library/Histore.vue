30438fa15e9644402af354c3dfd831afd70a9bd8
phuyalbish <phuyalbish@gmail.com>
2024-07-03 12:12:16 +0545

    nothing

---
+++ src/views/library/Histore.vue
@@ -0,0 +1,13 @@
+<template>
+  <PageLayoutWithPlayer id="display-flex">
+    <template #content>
+          ry  Histo
+    </template>
+  </PageLayoutWithPlayer>
+</template>
+
+<script>
+
+  
+
+</script>
---
+++ src/views/stats/StaffStats.vue
@@ -0,0 +1,31 @@
+<template>
+  <PageLayout>
+    <template #content>
+      <div class="  bg-dark-primary-color flex flex-col">
+        <MapShow />
+        <div class="flex flex-grow flex-wrap">
+          <AdminStats />
+          <AdminData />
+        </div>
+      
+      </div>
+    </template>
+  </PageLayout>
+</template>
+
+<script>
+import AdminData from '@/components/stats/admin/AdminData.vue';
+import AdminStats from '@/components/stats/admin/AdminStats.vue';
+
+import MapShow from '@/components/MapShow.vue';
+
+export default {
+  name: 'ArtistPage',
+  components: {
+    AdminData,
+    AdminStats,
+    
+    MapShow
+  }
+}
+</script>

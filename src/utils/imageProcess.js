c8f7f8a9563770aee24065373c9a9684f64f2915
phuyalbish <phuyalbish@gmail.com>
2024-07-06 20:35:10 +0545

    Album Updated

--- src/utils/imageProcess.js
+++ src/utils/imageProcess.js
@@ -1,5 +1,17 @@
+
+const base_url = import.meta.env.VITE_BASE_API_URL
 export const imgUrl = (image) => {
-    const base_url = import.meta.env.VITE_API_BASE_URL
+    if (image) {
+
+        console.log(base_url)
+
+        return `${base_url}/${image}`;
+    }
+    else
+        return "/public/favicon.png";
+};
+
+export const userimgUrl = (image) => {
     if (image) {
 
         console.log(base_url)
